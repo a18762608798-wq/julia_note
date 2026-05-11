a6985fd801ab9e61b08bb959d376c37e39707c40
git_usr <a18762608798@petalmail.com>
2026-05-11 18:10:30 +0800

    18-10

--- 06_projects/Mini_Project/test.jl
+++ 06_projects/Mini_Project/test.jl
@@ -1,3 +1,8 @@
 # this file use to test the environment of the project, and make sure all the dependencies are installed correctly.
+function main()
+    println("Hello, World!")
+end
 
-println("Hello, World!")
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
