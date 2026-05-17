6efc2a8217ff1e4c9fe9a63f353acf9b0b14ef57
git_usr <a18762608798@petalmail.com>
2026-05-17 15:04:53 +0800

    format all files with the style blue

--- 02_languages/06_moudles/99_TestModule.jl
+++ 02_languages/06_moudles/99_TestModule.jl
@@ -1,19 +1,19 @@
 module AnimalKingdom
-    export Animal, Mammal, Cat, Dog, speak # external interface.
+export Animal, Mammal, Cat, Dog, speak # external interface.
 
-    abstract type Animal end
-    abstract type Mammal <: Animal end
+abstract type Animal end
+abstract type Mammal <: Animal end
 
-    struct Cat <: Mammal
-        name::String
-        weight::Float64
-    end
+struct Cat <: Mammal
+    name::String
+    weight::Float64
+end
 
-    struct Dog <: Mammal
-        name::String
-        breed::String
-    end
+struct Dog <: Mammal
+    name::String
+    breed::String
+end
 
-    speak(a::Animal) = println("Some generic animal sound")
-    speak(c::Cat) = println("$(c.name) says meow!")
+speak(a::Animal) = println("Some generic animal sound")
+speak(c::Cat) = println("$(c.name) says meow!")
 end
--- 06_projects/Mini_Project/test.jl
+++ 06_projects/Mini_Project/test.jl
@@ -1,6 +1,6 @@
 # this file use to test the environment of the project, and make sure all the dependencies are installed correctly.
 function main()
-    println("Hello, World!")
+    return println("Hello, World!")
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
